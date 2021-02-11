0b0bf2df2c25470f185f02af2192ac215d39748e
okarin1511 <69676297+okarin1511@users.noreply.github.com>
2021-02-11 23:52:57 +0530

    Add files via upload

---
+++ Day-11/MinSwaps.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int minSwaps(int arr[], int n)
+{
+
+    pair<int, int> arrPos[n];
+    for (int i = 0; i < n; i++)
+    {
+        arrPos[i].first = arr[i];
+        arrPos[i].second = i;
+    }
+
+    sort(arrPos, arrPos + n);
+
+
+    vector<bool> vis(n, false);
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (vis[i] || arrPos[i].second == i)
+            continue;
+
+
+        int cycle_size = 0;
+        int j = i;
+        while (!vis[j])
+        {
+            vis[j] = 1;
+
+            j = arrPos[j].second;
+            cycle_size++;
+        }
+
+        if (cycle_size > 0)
+        {
+            ans += (cycle_size - 1);
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int arr[] = {1, 5, 4, 3, 2};
+    int n = (sizeof(arr) / sizeof(int));
+    cout << minSwaps(arr, n);
+    return 0;
+}
