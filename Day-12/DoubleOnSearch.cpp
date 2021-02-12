89c27e0aa2090a0a4480c868a842437d37590386
okarin1511 <69676297+okarin1511@users.noreply.github.com>
2021-02-12 16:29:05 +0530

    Add files via upload

---
+++ Day-12/DoubleOnSearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void Search(int arr[], int n, int b) {
+
+    for (int i=0; i < n; i++) {
+        if (b == arr[i]) {
+            b = b * 2;
+        }
+    }
+
+    cout<<b;
+}
+
+int main() {
+
+    int n ;
+    cout<<"enter n\n";
+    cin>>n;
+    int arr[n];
+    int b;
+
+
+    cout<<"enter elements:\n";
+    for(int i=0; i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"Enter b\n";
+    cin>> b;
+
+    Search(arr, n, b);
+
+    return 0;
+}
